a3ec53c517bf8ac987748546a136116e5af254d9
Milena <100636013+milena579@users.noreply.github.com>
2022-03-17 13:40:13 -0300

    Cadastro de negociação com sucesso
    Mostrar a mensagem de negociação feita com sucesso.

---
+++ app/views/view.ts
@@ -0,0 +1,20 @@
+export class View 
+{
+    protected elemento: HTMLElement;
+
+    constructor (seletor:string)
+    {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    update(model:string): void 
+    {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+
+    template(model:string): string
+    {
+        throw Error('Classe filha precisa implementar o método Template');
+    }
+}
